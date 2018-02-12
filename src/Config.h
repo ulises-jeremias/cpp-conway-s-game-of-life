e947488666df41bcf3e0a5cdfbe9758c5d45f8ab
ulises-jeremias <ulisescf.24@gmail.com>
2018-02-12 19:20:37 -0300

    Added Config header file

---
+++ src/Config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+struct Config
+{
+        unsigned windowWidth,
+                 windowHeight,
+                 simWidth,
+                 simHeight,
+                 quadSize;
+};
+
+#endif // CONFIG_H
